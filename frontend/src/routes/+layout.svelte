27216a1b0e94cb87953e0d956254a1af284f1a19
be-garam <be.garam.dev@gmail.com>
2024-06-25 15:47:15 +0900

    feat: Add header and footer to layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,28 @@
 <script>
 	import tabler from 'yesvelte/css/tabler.min.css?url'
+	import { Navbar, NavbarItem, El } from 'yesvelte'
 </script>
 <svelte:head>
 	<link rel='stylesheet' href={tabler}/>
 </svelte:head>
 
+<El container bgColor="light" borderBottom>
+	<El row alignItems="center" p='1'>
+		<El col="auto" m="3" me="auto" tag="h1">LOGO</El>
+		<El col="auto" m="3">
+			<El row>
+				<El col='auto' px="3">About</El>
+				<El col='auto' px="3">Tool</El>
+			</El>
+		</El>
+	</El>
+</El>
+
 <slot/>
+
+<El container>
+	<El row justifyContent="center" m="2">
+		<El col='auto'> This website is maintained by</El>
+		<El col='auto' p='0'> <a href="https://pnucolab.com/">Computational Omics Laboratory</a></El>
+	</El>
+</El>
