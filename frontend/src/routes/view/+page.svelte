3fdfb67260a3041241897bc32728c1639713e526
ssohuiKim <soooooh7@gmail.com>
2024-06-25 11:34:30 +0900

    csv파일 불러서 띄우기

---
+++ frontend/src/routes/view/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { parsedData } from '$lib/stores';
+    import { onMount } from 'svelte';
+    import { get } from 'svelte/store';
+  
+    let data = [];
+  
+    onMount(() => {
+      data = get(parsedData);
+      console.log(data);
+    });
+  </script>
+  
+  <div class="container">
+    <h1>Parsed CSV Data</h1>
+    {#if data.length > 0}
+      <table>
+        <thead>
+          <tr>
+            {#each Object.keys(data[0]) as key}
+              <th>{key}</th>
+            {/each}
+          </tr>
+        </thead>
+        <tbody>
+          {#each data as row}
+            <tr>
+              {#each Object.values(row) as value}
+                <td>{value}</td>
+              {/each}
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    {:else}
+      <p>No data available.</p>
+    {/if}
+  </div>
+  
