d84eec9341a1de81960b04d5a5255520232ed405
ssohuiKim <soooooh7@gmail.com>
2024-06-25 11:34:30 +0900

    csv파일 불러서 띄우기

--- frontend/src/lib/DrugChart.svelte
+++ frontend/src/lib/DrugChart.svelte
@@ -184,7 +184,7 @@
             writeDrugName();
             writeDate();
 
-
+            
             
         }
     }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,39 @@
-<script lang="ts">
+<script>
   import { Divider, El, FileUpload } from "yesvelte";
   import { Button, Icon } from 'yesvelte';
   import { goto } from '$app/navigation';
+  import Papa from 'papaparse';
+  import { parsedData } from '$lib/stores';
 
   let files_1;
   let hint_1 = "";
   let state_1 = void 0;
-  $:
-    console.log(files_1);
-  $:
-    if (files_1 && files_1[0] && files_1[0].type !== "text/csv") {
-      hint_1 = "File type should be csv";
-      state_1 = "invalid";
-    } else {
-      hint_1 = "";
-      state_1 = void 0;
-    }
+
+  $: console.log(files_1);
+
+  $: if (files_1 && files_1[0] && files_1[0].type !== "text/csv") {
+    hint_1 = "File type should be csv";
+    state_1 = "invalid";
+  } else {
+    hint_1 = "";
+    state_1 = void 0;
+  }
 
   function handleClick() {
-      goto('/result');
+    if (files_1 && files_1[0]) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const csvData = event.target.result;
+        Papa.parse(csvData, {
+          header: true,
+          complete: function(results) {
+            parsedData.set(results.data);
+            goto('/view');
+          }
+        });
+      };
+      reader.readAsText(files_1[0]);
+    }
   }
 </script>
 
@@ -26,8 +41,8 @@
   <El tag="strong">Choose a csv file</El>
   <FileUpload mt="2" state={state_1} bind:files={files_1} />
   <El textColor="danger" tag="small">{hint_1}</El>
-  <Button color="warning" on:click={handleClick}>
-    <Icon name="heart" />I like
+  <Button color="light" on:click={handleClick}>
+    <Icon name="upload" />Upload
   </Button>
 </div>
 
