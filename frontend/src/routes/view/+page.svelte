93844329e3771422a8d47d4f7ccb5a391f590d98
ssohuiKim <soooooh7@gmail.com>
2024-06-26 13:38:07 +0900

    ICI Drugchart에 적용

--- frontend/src/routes/view/+page.svelte
+++ frontend/src/routes/view/+page.svelte
@@ -40,44 +40,35 @@
 
    <script>
     import { onMount } from 'svelte';
-    import { drugExposureStartDates } from '$lib/stores'; // Import the specific store
+    import { drugConceptIds } from '$lib/stores'; // Import the specific store
   
-    let uniqueDates = [];
-    let formattedDates = [];
+    let iciList = [];
   
-    function formatDate(dateString) {
-      const [year, month, day] = dateString.split('-').map(part => part.padStart(2, '0'));
-      return `${year}-${month}-${day}`;
-    }
+    // Map specific drug concept IDs to corresponding letters
+    const idToLetterMap = {
+      42920398: 'Atezolizumab',
+      1594046: 'Durvalumab',
+      1594038: 'Durvalumab',
+      46275962: 'Ipilimumab',
+      42920744: 'Nivolumab',
+      42922127: 'Nivolumab',
+      42921578: 'Pembrolizumab'
+    };
   
     onMount(() => {
-      drugExposureStartDates.subscribe(dates => {
-        const dateSet = new Set(dates.filter(date => date)); // Filter out empty values
-        uniqueDates = Array.from(dateSet); // Convert Set back to Array
-  
-        if (uniqueDates.length > 0) {
-          // Format the first date with year-month-day
-          formattedDates = [formatDate(uniqueDates[0])];
-  
-          // Format the rest of the dates without the year
-          for (let i = 1; i < uniqueDates.length; i++) {
-            const [year, month, day] = uniqueDates[i].split('-').map(part => part.padStart(2, '0'));
-            formattedDates.push(`${month}.${day}`);
-          }
-        }
-        console.log("Formatted Drug Exposure Start Dates:", formattedDates);
+      drugConceptIds.subscribe(ids => {
+        // Filter the IDs and map to corresponding letters
+        iciList = Array.from(new Set(ids.filter(id => idToLetterMap[id])
+               .map(id => idToLetterMap[id])));
+        console.log("ICI List:", iciList);
       });
     });
   </script>
   
   <div class="container">
-    <h1>Unique Drug Exposure Start Dates</h1>
-    {#if formattedDates.length > 0}
-      <ul>
-        {#each formattedDates as date}
-          <li>{date}</li>
-        {/each}
-      </ul>
+    <h1>ICI List</h1>
+    {#if iciList.length > 0}
+        <li>{iciList}</li>
     {:else}
       <p>No data available.</p>
     {/if}
