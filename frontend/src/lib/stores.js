41901235822aa3055e7c4125af287fb3f922f0e2
ssohuiKim <soooooh7@gmail.com>
2024-06-26 10:33:24 +0900

    patient의 date chart에 적용

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -1,3 +1,16 @@
+
 import { writable } from 'svelte/store';
 
 export const parsedData = writable([]);
+export const patientNos = writable([]);
+export const subs = writable([]);
+export const indexDates = writable([]);
+export const followEnds = writable([]);
+export const indexLastdates = writable([]);
+export const visitLastDays = writable([]);
+export const drugExposureStartDates = writable([]);
+export const drugConceptIds = writable([]);
+export const drugNames = writable([]);
+export const drugNameDoses = writable([]);
+export const measurementDates = writable([]);
+export const grades = writable([]);
