c9bba8d95ce64f22dd09828789b2ef6377173469
be-garam <be.garam.dev@gmail.com>
2024-06-27 22:06:08 +0900

    fix: handle patientnum hdm error

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 
 <script>
-  import { Divider, El, FileUpload } from "yesvelte";
-  import { Button, Icon } from 'yesvelte';
+  import { Card, CardBody, CardFooter, El, Button, CardActions, FileUpload, Icon } from "yesvelte";
+  // import { Button, Icon } from 'yesvelte';
   import { goto } from '$app/navigation';
   import Papa from 'papaparse';
   import {
@@ -82,14 +82,25 @@
   }
 </script>
 
-<div class="container">
-  <El tag="strong">Choose a csv file</El>
-  <FileUpload mt="2" state={state_1} bind:files={files_1} />
-  <El textColor="danger" tag="small">{hint_1}</El>
-  <Button color="light" on:click={handleClick}>
-    <Icon name="upload" />Upload
-  </Button>
-</div>
-
-<slot/>
-
+<El container m="0" p="4">
+  <El row tag="h1">CDM-Review</El>
+  <El row tag="h3">CDM-Review is a web tool that visualizes data obtained from CDM on patients taking immune checkpoint inhibitors who showed Hepatotoxicity.<br> 
+    It would help medical professionals identify patients and make medication-related decisions.</El>
+  <El row style="margin-top: 52px;">
+    <Card col="7">
+      <CardBody>
+        <El row tag="strong">Choose a csv file</El>
+        <FileUpload mt="2" state={state_1} bind:files={files_1} />
+        <El textColor="danger" tag="small">{hint_1}</El>
+      </CardBody>
+      <CardFooter>
+        <CardActions>
+          <Button color="primary" on:click={handleClick}>
+            <Icon name="upload" />Upload
+          </Button>
+        </CardActions>
+      </CardFooter>
+    </Card>
+  </El>
+  <!-- <Button color="primary" on:click={() => goto('/result')}>View</Button> -->
+</El>
