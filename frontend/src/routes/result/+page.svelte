bd3b0ad87f4a48b586f98dd24483f29239163775
be-garam <be.garam.dev@gmail.com>
2024-06-27 22:06:08 +0900

    fix: handle patientnum hdm error

--- frontend/src/routes/result/+page.svelte
+++ frontend/src/routes/result/+page.svelte
@@ -1,35 +1,121 @@
 <script>
 	import {
-		TabList,
-		TabItem,
-		TabContent,
-		TabPanel,
-		Tabs,
-		Card,
-		CardHeader,
-		CardBody,
+		El,
+		Button, 
+		Modal, ModalBody, ModalFooter
 	} from 'yesvelte'
+
+	import { onMount } from 'svelte';
+
     import DrugChart from '../../lib/DrugChart.svelte'
+	import { patientNos } from '$lib/stores'
+
+	let show = false;
+	let patients = [];
+	let selectedPatient = null;
+
+	onMount(() => {
+		// want to get patientNos from the store and filter out the unique values and empty strings
+		patientNos.subscribe(value => {
+			patients = Array.from(new Set(value)).filter(Boolean);
+		});
+	});
+
+	function selectPatient(patientNum) {
+		selectedPatient = patientNum;
+		console.log('Selected Patient:', selectedPatient);
+	}
 </script>
 
-<Card>
-	<Tabs>
-		<CardHeader>
-			<TabList>
-				<TabItem active>Home</TabItem>
-				<TabItem>Profile</TabItem>
-			</TabList>
-		</CardHeader>
-		<CardBody>
-			<TabContent>
-				<TabPanel>
-					<DrugChart />
-				</TabPanel>
-				<TabPanel>
-					<h4>Profile tab</h4>
-					Here is the content of the second tab.
-				</TabPanel>
-			</TabContent>
-		</CardBody>
-	</Tabs>
-</Card>
+
+<style>
+    .img-button {
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+
+    .img-button img {
+        width: 20px; 
+        height: 20px; 
+    }
+
+	.card {
+		background-color: white;
+		border-radius: 4px;
+		border-width: 1px;
+		border-color: #DCE0E5;
+		border-style: solid; 
+		padding: 16px;
+		display: flex;
+		flex-direction: row;
+		height: 100%;
+		overflow: auto;
+	}
+
+	.sidebar {
+		border-right: 1px solid #dcdcdc;
+		width: 150px;
+		overflow: auto;
+		padding-right: 12px;
+		flex-shrink: 0;
+	}
+
+	.sidebar button {
+		display: block;
+		width: 100%;
+		margin-bottom: 8px;
+		padding: 8px;
+		background-color: white;
+		border: 0px;
+		text-align: left;
+		cursor: pointer;
+	}
+
+	.sidebar button:hover {
+		background-color: #e0e0e0;
+	}
+	
+	.scroll-container {
+		flex: 1;
+		overflow: auto;
+	}
+</style>
+<El container m="0" p="4" style="height: 100%;">
+	<El row alignItems="start" p="0" m="0">
+		<El col='auto' tag="h1" p="0" m="0">Results</El>
+		<El col='auto' alignSelf="center">
+			<button class="img-button" on:click={() => (show = !show)}>
+				<img src="/tooltip.svg" alt="Tooltip Icon">
+			</button>
+		</El>
+	</El>
+	<El row style="margin-top: 24px; height: 100%">
+		<div class="card">
+			<div class="sidebar">
+				{#each patients as patientNum}
+					<button on:click={() => selectPatient(patientNum)}>
+						Patient {patientNum}
+					</button>
+				{/each}
+			</div>
+			<div class="scroll-container">
+				<DrugChart/>
+			</div>
+		</div>
+	</El>
+</El>
+
+<Modal title="Hepatotoxic Drug" bind:show>
+	<ModalBody>
+		Currently, hepatotoxic drugs are classified according to the National Library of Medicine (NIH)'s Master List of LiverTox Drugs (October 2023).
+		<br><br>
+		LiverTox: Clinical and Research Information on Drug-Induced Liver Injury [Internet]. Bethesda (MD): National Institute of Diabetes and Digestive and Kidney Diseases; 2012-. Master List of LiverTox Drugs. [Updated 2023 Feb 10]. 
+		<br><br>
+		Available from: <a target="_blank" href="https://www.ncbi.nlm.nih.gov/books/NBK571102/">here</a>
+	</ModalBody>
+	<ModalFooter>
+		<!-- <Button me="auto" on:click={() => (show = false)}>Close</Button> -->
+		<Button color="primary" on:click={() => (show = false)}>Checked</Button>
+	</ModalFooter>
+</Modal>
