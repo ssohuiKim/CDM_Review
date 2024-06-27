c7ece0b93c8ffd2de2cdee613e86d43411b42ea6
ssohuiKim <soooooh7@gmail.com>
2024-06-27 14:14:40 +0900

    grade에 따른 이미지 변경

--- frontend/src/routes/view/+page.svelte
+++ frontend/src/routes/view/+page.svelte
@@ -37,14 +37,16 @@
     {/if}
   </div> -->
   
-
-<script>
-  import {onMount} from 'svelte';
-  import {drugConceptIds, drugNames, drugExposureStartDates} from '$lib/stores';
+  <script>
+    import {onMount} from 'svelte';
+    import {drugConceptIds, drugNames, drugExposureStartDates, measurementDates, grades} from '$lib/stores';
+  
     let takenDrugs = [];
     let drugExposureDates = [];
     let uniqueDates = [];
-
+    let measurements = [];
+    let hepatoxicityGrades = [];
+  
     let toxic = [];
     let formattedDates = [];
     let drugs = [];
@@ -57,83 +59,70 @@
       42922127: 'nivolumab',
       42921578: 'pembrolizumab'
     };
-
+  
     function formatDate(dateString) {
-        const [year, month, day] = dateString.split('-').map(part => part.padStart(2, '0'));
-        return `${year}.${month}.${day}`;
+      const [year, month, day] = dateString.split('-').map(part => part.padStart(2, '0'));
+      return `${year}.${month}.${day}`;
     }
-
+  
     onMount(() => {
-        drugExposureStartDates.subscribe(dates => {
-            drugExposureDates = dates.filter(date => date); // Filter out empty values
-            const dateSet = new Set(drugExposureDates);
-            uniqueDates = Array.from(dateSet); // Convert Set back to Array
-            
-            if (uniqueDates.length > 0) {
-                formattedDates = [formatDate(uniqueDates[0])];
-                for (let i = 1; i < uniqueDates.length; i++) {
-                const [year, month, day] = uniqueDates[i].split('-').map(part => part.padStart(2, '0'));
-                formattedDates.push(`${month}.${day}`);
-                }
-            }
-            
-        });
-        
-        drugNames.subscribe(data => {
-            takenDrugs = data.filter(name => name); 
-            toxic = Array.from(new Set(takenDrugs));
-        });
+      drugExposureStartDates.subscribe(dates => {
+        drugExposureDates = dates.filter(date => date); // Filter out empty values
+        const dateSet = new Set(drugExposureDates);
+        uniqueDates = Array.from(dateSet); // Convert Set back to Array
+  
+        if (uniqueDates.length > 0) {
+          formattedDates = [formatDate(uniqueDates[0])];
+          for (let i = 1; i < uniqueDates.length; i++) {
+            const [year, month, day] = uniqueDates[i].split('-').map(part => part.padStart(2, '0'));
+            formattedDates.push(`${month}.${day}`);
+          }
+        }
+      });
+  
+      drugNames.subscribe(data => {
+        takenDrugs = data.filter(name => name);
+        toxic = Array.from(new Set(takenDrugs));
+      });
+  
+      drugConceptIds.subscribe(ids => {
+        drugs = Array.from(new Set(ids.filter(id => idToDrugMap[id])
+          .map(id => idToDrugMap[id])));
+      });
+  
+      measurementDates.subscribe(data => {
+        measurements = data.filter(date => date);
+      });
 
-        drugConceptIds.subscribe(ids => {
-            drugs = Array.from(new Set(ids.filter(id => idToDrugMap[id])
-               .map(id => idToDrugMap[id])));
-        });
+      grades.subscribe(data => {
+            hepatoxicityGrades = data.filter(grade => grade); // Filter out empty values
+      });
+
     });
-
-
+  
     const getToxicIndex = (name) => toxic.indexOf(name) + 1;
     const getDateIndex = (date) => uniqueDates.indexOf(date) + 1;
     const getDrugIndex = (drug) => drugs.indexOf(drug) + 1;
-    // const getDrugIndexInTakenDrugs = (drug) => takenDrugs.indexOf(drug) + 1;
     const getDrugIndexInTakenDrugs = (drug) => {
-    let indices = [];
-    takenDrugs.forEach((item, index) => {
-      if (item.toLowerCase() === drug.toLowerCase()) {
-        indices.push(index);
-      }
-    });
-    return indices;
-  }
-</script>
-
+      let indices = [];
+      takenDrugs.forEach((item, index) => {
+        if (item.toLowerCase() === drug.toLowerCase()) {
+          indices.push(index);
+        }
+      });
+      return indices;
+    }
+  </script>
+  
 <div class="container">
-  <h1>Unique Drug Names and Dates</h1>
+
+  <h1>Measurement Dates and Hepatoxicity Grades</h1>
   <ul>
-    {#each drugs as drug}
-      <p>{drug}</p>
-      {#each getDrugIndexInTakenDrugs(drug) as index}
-        <li>
-          {drug} - (Drug Index in Drugs: {getDrugIndex(drug)})
-          - (Drug Index in TakenDrugs: {index + 1})
-          - (Date Index: {getDateIndex(drugExposureDates[index])})
-          - (UniqueDate: {formattedDates[getDateIndex(drugExposureDates[index]) - 1]})
-        </li>
-      {/each}
+    {#each measurements as measurement, index}
+      <li>
+        {measurement} - (Date Index: {getDateIndex(measurement)})
+      </li>
     {/each}
   </ul>
-
-  <!-- {#if takenDrugs.length > 0 && drugExposureDates.length > 0}
-    <ul>
-      {#each takenDrugs as drug, i}
-        {#if drugExposureDates[i]} 
-          <li>
-            {drug} - {drugExposureDates[i]} 
-            (Toxic Index: {getToxicIndex(drug)}, Date Index: {getDateIndex(drugExposureDates[i])})
-          </li>
-        {/if}
-      {/each}
-    </ul>
-  {:else}
-    <p>No data available.</p>
-  {/if} -->
 </div>
+  
