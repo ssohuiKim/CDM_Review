fc276dbaa917bf72c3ef6b619ab254a2adf77cff
sohui Kim <soooooh7@gmail.com>
2024-07-02 14:06:08 +0900

    duckdb html error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,25 @@
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	plugins: [sveltekit()]
+// });
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    createHtmlPlugin() // HTML 플러그인 추가
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 });
