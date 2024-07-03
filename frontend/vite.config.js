575f87ea1fa08392d621c6758e19bd79789be990
sohui Kim <soooooh7@gmail.com>
2024-07-03 16:04:10 +0900

    state d408ba5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,6 @@
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-
-
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    createHtmlPlugin() // HTML 플러그인 추가
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
+	plugins: [sveltekit()]
 });
