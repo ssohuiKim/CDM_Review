c730ba9949fc9efd38b61c5de8cd19ec08c46454
sohui Kim <soooooh7@gmail.com>
2024-07-10 15:52:41 +0900

    spinner 추가

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,10 +3,13 @@
   import { goto } from '$app/navigation';
   import { parsedData, groupedPatientData } from '$lib/stores';
   import { initializeDuckDB } from '$lib/duckdb';
+  import Spinner from '$lib/components/Spinner.svelte';
+  import { writable } from 'svelte/store';
 
   let files_1;
   let hint_1 = "";
   let state_1 = void 0;
+  const loading = writable(false); // 로딩 상태를 관리하는 변수
 
   $: console.log(files_1);
 
@@ -35,6 +38,7 @@
 
   async function handleClick() {
     if (files_1 && files_1[0]) {
+      loading.set(true); // 로딩 시작
       const db = await initializeDuckDB();
       const reader = new FileReader();
       reader.onload = async (event) => {
@@ -46,9 +50,6 @@
         await connection.query(`
           CREATE TABLE patients AS SELECT * FROM read_csv_auto('data.csv');
         `);
-        // await connection.query(`
-        //   CREATE TABLE patients AS SELECT * FROM read_csv('data.csv', delim=',', header=true, columns={Patient_no: 'STRING', drug_concept_id: 'STRING', drug_exposure_start_date: 'STRING', drug_name: 'STRING', drug_name_dose: 'STRING', follow_end: 'STRING', grade: 'STRING', index_date: 'STRING', index_lastdate: 'STRING', measurement_date: 'STRING', sub: 'STRING', visit_last_Day: 'STRING'});
-        // `);
 
         // 파싱된 데이터 가져오기
         const result = await connection.query(`SELECT * FROM patients`);
@@ -67,8 +68,7 @@
 
         // 그룹화된 데이터 스토어에 업데이트
         groupedPatientData.set(groupedData);
-
-        // 결과 페이지로 이동
+        loading.set(false); // 로딩 종료
         goto('/result');
       };
       reader.readAsText(files_1[0]);
@@ -76,24 +76,29 @@
   }
 </script>
 
-<El container m="0" p="4">
-  <El row tag="h1">CDM-Review</El>
-  <El row tag="h3">CDM-Review is a web tool that visualizes data obtained from CDM on patients taking immune checkpoint inhibitors who showed Hepatotoxicity.<br> 
-    It would help medical professionals identify patients and make medication-related decisions.</El>
-  <El row style="margin-top: 52px;">
-    <Card col="7">
-      <CardBody>
-        <El row tag="strong">Choose a csv file</El>
-        <FileUpload mt="2" state={state_1} bind:files={files_1} />
-        <El textColor="danger" tag="small">{hint_1}</El>
-      </CardBody>
-      <CardFooter>
-        <CardActions>
-          <Button color="primary" on:click={handleClick}>
-            <Icon name="upload" />Upload
-          </Button>
-        </CardActions>
-      </CardFooter>
-    </Card>
+{#if $loading}
+  <Spinner/>
+{:else}
+  <El container m="0" p="4">
+    <El row tag="h1">CDM-Review</El>
+    <El row tag="h3">CDM-Review is a web tool that visualizes data obtained from CDM on patients taking immune checkpoint inhibitors who showed Hepatotoxicity.<br> 
+      It would help medical professionals identify patients and make medication-related decisions.</El>
+    <El row style="margin-top: 52px;">
+      <Card col="7">
+        <CardBody>
+          <El row tag="strong">Choose a csv file</El>
+          <FileUpload mt="2" state={state_1} bind:files={files_1} />
+          <El textColor="danger" tag="small">{hint_1}</El>
+        </CardBody>
+        <CardFooter>
+          <CardActions>
+            <Button color="primary" on:click={handleClick}>
+              <Icon name="upload" />Upload
+            </Button>
+          </CardActions>
+        </CardFooter>
+      </Card>
+    </El>
   </El>
-</El>
+{/if}
+
