68fbd0c72cb165e405bc09f7b8532ce5e7d8e915
sohui Kim <soooooh7@gmail.com>
2024-07-19 14:30:43 +0900

    download all charts

--- frontend/src/routes/result/+page.svelte
+++ frontend/src/routes/result/+page.svelte
@@ -3,19 +3,22 @@
 	  El,
 	  Button, 
 	  Modal, ModalBody, ModalFooter
-	} from 'yesvelte'
+	} from 'yesvelte';
   
 	import { onMount } from 'svelte';
 	import DrugChart from '../../lib/DrugChart.svelte';
 	import { groupedPatientData } from '$lib/stores';
+	import html2canvas from 'html2canvas';
+	import JSZip from 'jszip';
+	import { saveAs } from 'file-saver';
   
 	let show = false;
+	let showLoading = false; // 로딩 모달 상태
 	let patients = [];
 	let selectedPatient = null;
 	let patientData = {};
-  
+	
 	onMount(() => {
-	  // Get groupedPatientData from the store
 	  groupedPatientData.subscribe(data => {
 		patients = Object.keys(data);
 		patientData = data;
@@ -26,9 +29,37 @@
 	  selectedPatient = patientNum;
 	  console.log('Selected Patient:', selectedPatient);
 	}
-  </script>
   
-  <style>
+	async function downloadAllCharts() {
+	  showLoading = true; // 다운로드 시작 시 로딩 모달 표시
+	  const zip = new JSZip();
+	  for (const patientNum of patients) {
+		const chartContainer = document.createElement('div');
+		chartContainer.style.position = 'absolute';
+		chartContainer.style.left = '-9999px'; // 화면에서 보이지 않도록 함
+		document.body.appendChild(chartContainer);
+  
+		const chart = new DrugChart({
+		  target: chartContainer,
+		  props: { selectedPatient: patientNum, patientData }
+		});
+  
+		await new Promise(resolve => setTimeout(resolve, 500)); // 차트가 렌더링될 시간을 기다림
+  
+		const canvas = chartContainer.querySelector('canvas');
+		const dataUrl = await html2canvas(canvas).then(canvas => canvas.toDataURL('image/png'));
+  
+		zip.file(`patient-${patientNum}.png`, dataUrl.split(',')[1], { base64: true });
+		chart.$destroy();
+		document.body.removeChild(chartContainer);
+	  }
+  
+	  const content = await zip.generateAsync({ type: 'blob' });
+	  saveAs(content, 'charts.zip');
+	  showLoading = false; // 다운로드 완료 시 로딩 모달 숨기기
+	}
+</script>
+<style>
 	.img-button {
 	  background: none;
 	  border: none;
@@ -36,8 +67,8 @@
 	}
   
 	.img-button img {
-	  width: 20px; 
-	  height: 20px; 
+	  width: 20px;
+	  height: 20px;
 	}
   
 	.card {
@@ -75,14 +106,42 @@
 	.sidebar button:hover {
 	  background-color: #e0e0e0;
 	}
-	
+  
 	.scroll-container {
 	  flex: 1;
 	  overflow: auto;
 	}
-  </style>
   
-  <El container m="0" p="4" style="height: 100%;">
+	.text-button {
+	  background: none;
+	  border: none;
+	  cursor: pointer;
+	  font-size: 14px;
+	  padding: 0 8px;
+	}
+  
+	.loading-modal {
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+	  background: rgba(0, 0, 0, 0.5);
+	  z-index: 9999;
+	}
+  
+	.loading-content {
+	  background: white;
+	  padding: 20px;
+	  border-radius: 5px;
+	  text-align: center;
+	}
+</style>
+
+<El container m="0" p="4" style="height: 100%;">
 	<El row alignItems="start" p="0" m="0">
 	  <El col='auto' tag="h1" p="0" m="0">Results</El>
 	  <El col='auto' alignSelf="center">
@@ -90,37 +149,53 @@
 		  <img src="/tooltip.svg" alt="Tooltip Icon">
 		</button>
 	  </El>
-	</El>
-	<El row style="margin-top: 24px; height: 100%">
-	  <div class="card">
-		<div class="sidebar">
-		  {#each patients as patientNum}
-			<button on:click={() => selectPatient(patientNum)}>
-			  Patient {patientNum}
-			</button>
-		  {/each}
-		</div>
-		<div class="scroll-container">
-		  {#if selectedPatient !== null}
-			<DrugChart {selectedPatient} {patientData} />
-		  {:else}
-			<p>Please select a patient to view their data.</p>
-		  {/if}
-		</div>
-	  </div>
-	</El>
-  </El>
   
-  <Modal title="Hepatotoxic Drug" bind:show>
-	<ModalBody>
-	  Currently, hepatotoxic drugs are classified according to the National Library of Medicine (NIH)'s Master List of LiverTox Drugs (October 2023).
-	  <br><br>
-	  LiverTox: Clinical and Research Information on Drug-Induced Liver Injury [Internet]. Bethesda (MD): National Institute of Diabetes and Digestive and Kidney Diseases; 2012-. Master List of LiverTox Drugs. [Updated 2023 Feb 10]. 
-	  <br><br>
-	  Available from: <a target="_blank" href="https://www.ncbi.nlm.nih.gov/books/NBK571102/">here</a>
-	</ModalBody>
-	<ModalFooter>
-	  <Button color="primary" on:click={() => (show = false)}>Checked</Button>
-	</ModalFooter>
-  </Modal>
+	  <!-- 임시버튼 -->
+	  <El col='auto' alignSelf="center">
+		<button class="text-button" on:click={downloadAllCharts}>
+		  Download All Charts
+		</button>
+	  </El>
+	   <!-- 임시버튼 -->
+</El>
+<El row style="margin-top: 24px; height: 100%">
+	<div class="card">
+	<div class="sidebar">
+		{#each patients as patientNum}
+		<button on:click={() => selectPatient(patientNum)}>
+			Patient {patientNum}
+		</button>
+		{/each}
+	</div>
+	<div class="scroll-container">
+		{#if selectedPatient !== null}
+		<DrugChart {selectedPatient} {patientData} />
+		{:else}
+		<p>Please select a patient to view their data.</p>
+		{/if}
+	</div>
+	</div>
+</El>
+
+{#if showLoading}
+	<div class="loading-modal">
+	<div class="loading-content">
+		<p>Downloading charts, please wait...</p>
+	</div>
+	</div>
+{/if}
+</El>
+
+<Modal title="Hepatotoxic Drug" bind:show>
+<ModalBody>
+	Currently, hepatotoxic drugs are classified according to the National Library of Medicine (NIH)'s Master List of LiverTox Drugs (October 2023).
+	<br><br>
+	LiverTox: Clinical and Research Information on Drug-Induced Liver Injury [Internet]. Bethesda (MD): National Institute of Diabetes and Digestive and Kidney Diseases; 2012-. Master List of LiverTox Drugs. [Updated 2023 Feb 10]. 
+	<br><br>
+	Available from: <a target="_blank" href="https://www.ncbi.nlm.nih.gov/books/NBK571102/">here</a>
+</ModalBody>
+<ModalFooter>
+	<Button color="primary" on:click={() => (show = false)}>Checked</Button>
+</ModalFooter>
+</Modal>
   
